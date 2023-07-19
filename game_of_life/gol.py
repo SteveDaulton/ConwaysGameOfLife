b5dfeaa4307858f6b8de6d2be0fe84021c3a27eb
Steve Daulton <stevedaulton@gmail.com>
2023-07-19 22:39:15 +0100

    General tidying and TODO notes.

--- game_of_life/gol.py
+++ game_of_life/gol.py
@@ -29,13 +29,14 @@ class Size(NamedTuple):
 
 
 class Preset(NamedTuple):
+    """Preset initial state of universe."""
     idx: int
     name: str
     cells: set[Point]
 
 
 def get_preset(choice: int = -1) -> Preset | list[Preset]:
-    """Return preset dictionary or specified preset when choice >=0.
+    """Return preset dictionary or specified preset when valid choice passed.
     Examples from:
     https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
     """
@@ -48,10 +49,14 @@ def get_preset(choice: int = -1) -> Preset | list[Preset]:
         Preset(3, 'glider', {Point(2, 3), Point(3, 4), Point(4, 2), Point(4, 3), Point(4, 4)}),
         Preset(4, 'R-pentomino', {Point(10, 51), Point(10, 52), Point(11, 50),
                                   Point(11, 51), Point(12, 51)}),
-        Preset(5, 'random', set(Point(randint(0, 38), randint(0, 98)) for _ in range(randint(10, 1000))))
+        # TODO: Use Universe size rather than hard coded ranges for y and x.
+        Preset(5, 'random', set(Point(randint(0, 38), randint(0, 98))
+                                for _ in range(randint(10, 1000))))
     ]
     if choice >= 0:
-        return presets[choice]
+        if choice < len(presets):
+            return presets[choice]
+        sys.exit('Invalid Preset ID.')
     return presets
 
 
@@ -63,24 +68,26 @@ def display_menu():
 
 
 def get_user_choice():
+    """Return user choice from preset menu."""
     valid_indices = {option.idx for option in get_preset()}
     while True:
         try:
             choice = int(input("Select initial state: "))
             if choice in valid_indices:
                 return choice
-            else:
-                print("Invalid choice. Please try again.")
+            print("Invalid choice. Please try again.")
         except IndexError:
             print("Invalid input. Please enter a number.")
 
 
-def universe_init(choice: int = 4) -> set[Point]:
+def universe_init(choice: int) -> set[Point]:
     """Return initial universe state.
     Override this function for custom initial state.
     """
+    # TODO: Make Universe a class to handle global properties
+    # TODO: such as initial state, size of pad, colours, etc.
     # Keep MyPy happy.
-    assert isinstance((live_cells := get_preset(choice)), Preset)
+    assert isinstance((live_cells := get_preset(choice)), Preset), f'Invalid preset id {choice}'
     return live_cells.cells
 
 
@@ -126,10 +133,13 @@ def update(living: set[Point]) -> set[Point]:
     return new_cells
 
 
-def cell_in_range(cell):
-    return (99 >= cell[0] >= 0 and
-            99 >= cell[1] >= 0 and
-            sum(cell) < 198)
+def cell_in_range(cell: Point) -> bool:
+    """Return True if cell is within range of pad."""
+    # TODO: Get pad size from Universe rather than duplicating setting.
+    pad_size: Size = Size(y=40, x=100)
+    return (pad_size.y - 1 >= cell.y >= 0 and
+            pad_size.x - 1 >= cell.x >= 0 and not
+            (cell.y == pad_size.y - 1 and cell.x == pad_size.x - 1))
 
 
 def refresh_pad(pad, size):
@@ -150,12 +160,14 @@ def main(stdscr: curses.window, choice: int) -> None:
     universe: set[Point] = universe_init(choice)
     universe_old: set[Point] = set()
     # Pad (y, x) must be big enough to hold content.
+    # TODO: This should be part of Universe settings.
     pad_size: Size = Size(y=40, x=100)
     pad = curses.newpad(pad_size[0], pad_size[1])
 
     # Initialise timer.
     clock = perf_counter()
     # If refresh rate is too fast, then animation will not be smooth.
+    # TODO: This should be part of Universe settings.
     refresh_rate: Final[float] = 0.1
 
     while True:
@@ -165,6 +177,7 @@ def main(stdscr: curses.window, choice: int) -> None:
             pad.addch(y, x, ' ')
 
         # Add content to pad in reverse colours
+        # TODO: Validate cells in Universe initial state.
         for y, x in universe:
             pad.addch(y, x, ' ', curses.A_REVERSE)
 
@@ -178,8 +191,6 @@ def main(stdscr: curses.window, choice: int) -> None:
 
 
 if __name__ == '__main__':
-    default_preset = 4
-
     if len(sys.argv) == 1:
         # No arguments were passed
         display_menu()
@@ -195,9 +206,7 @@ if __name__ == '__main__':
         args = parser.parse_args()
         try:
             preset = get_preset(args.preset)
-            print('HERE', preset)
         except IndexError:
             sys.exit(f'Invalid preset number. Select a preset from 0 to {len(get_preset())}')
-        print('AND HERE?')
         partial_main = partial(main, choice=args.preset)
         curses.wrapper(partial_main)
