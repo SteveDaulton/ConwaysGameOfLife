3eeeace045b34d3669e156a1385fefe9531c7bf1
Steve Daulton <stevedaulton@gmail.com>
2023-08-15 18:13:11 +0100

    Improvements to test_main.
    Tests start with more general assertions about the
    function calls, and then focus on more specific
    details of the function arguments.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
 """Tests for __main__.py."""
 from unittest.mock import patch
+from functools import partial
 
 from game_of_life.__main__ import main
 from game_of_life.gol import get_all_presets
@@ -33,26 +34,40 @@ def test_main(monkeypatch) -> None:
     monkeypatch.setattr('sys.argv', ['__main__.py'])
 
     # Mock the functions that main calls.
-    with patch('game_of_life.__main__.display_menu') as mock_display_menu, \
-         patch('game_of_life.__main__.get_user_choice') as mock_get_user_choice, \
+    with patch('game_of_life.__main__.preset_menu') as mock_preset_menu, \
+         patch('game_of_life.__main__.refresh_rate_menu') as mock_refresh_rate_menu, \
          patch('curses.wrapper') as mock_curses_wrapper, \
          patch('game_of_life.__main__.play') as mock_play:
 
-        # Mock user choice of preset.
-        mock_get_user_choice.return_value = 4
-
-        # Set the return_value of mock_play to a partial object.
-        # mock_play.return_value = partial(mock_play, choice='1', refresh_rate=0.5)
+        # Mock user input.
+        mock_preset_menu.return_value = 4
+        mock_refresh_rate_menu.return_value = 0.5
 
         main()
 
-        print('\nArgs:', mock_curses_wrapper.call_args)
-        print('\nArgs:', mock_curses_wrapper.call_args.args[0])
-        print('Expect:', mock_play.return_value)
+        # Get the arguments passed to the mock_curses_wrapper function call.
+        wrapper_args = mock_curses_wrapper.call_args.args[0]
 
-        # Assert that the mocked functions were called with the expected parameters.
-        mock_display_menu.assert_called_once()
-        mock_get_user_choice.assert_called_once()
-        # mock_curses_wrapper.assert_called_once_with(
-        #     mock_curses_wrapper.call_args[0][0])
-        mock_curses_wrapper.assert_called_once()
+        # Check function calls.
+
+        # Assert that the mocked menu functions were called once.
+        mock_preset_menu.assert_called_once()
+        mock_refresh_rate_menu.assert_called_once()
+
+        # Assert that mock_curses_wrapper was called with the expected args.
+        mock_curses_wrapper.assert_called_once_with(wrapper_args)
+
+        # Assert mock_curses_wrapper was called with correct keyword arguments.
+        assert (mock_curses_wrapper.call_args.args[0].keywords['choice'] ==
+                mock_preset_menu.return_value)
+        assert (mock_curses_wrapper.call_args.args[0].keywords['refresh_rate'] ==
+                mock_refresh_rate_menu.return_value)
+
+        # Set the return_value of mock_play to a partial object.
+        expected_partial = partial(mock_play, choice=mock_preset_menu.return_value,
+                                   refresh_rate=mock_refresh_rate_menu.return_value)
+
+        # Assert that mock_curses_wrapper is called with expected partial.
+        assert isinstance(wrapper_args, partial)
+        assert wrapper_args.func == expected_partial.func
+        assert wrapper_args.keywords == expected_partial.keywords
