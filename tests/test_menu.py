d7e90037e1d9ad4fa2c3f8e5c9c17c5ae86906ed
Steve Daulton <stevedaulton@gmail.com>
2023-08-15 20:27:06 +0100

    Fix test_preset_menu_with_presets.
    The test_preset_menu_with_presets was previously
    called test_display_menu_with_presets.

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -5,11 +5,11 @@ from unittest.mock import patch
 import pytest
 
 from game_of_life.custom_types import Preset
-from game_of_life.menu import display_menu
+from game_of_life.menu import preset_menu
 
 
 @pytest.mark.parametrize(
-    "test_settings,expected_output",[
+    "test_settings,expected_output", [
         # Empty list of Presets will print nothing.
         ([], ''),
         # List of one preset
@@ -19,17 +19,22 @@ from game_of_life.menu import display_menu
           Preset(1, 'Second', set()),
           Preset(2, 'Third', set())],
          '0. First\n1. Second\n2. Third')])
-def test_display_menu_with_fixture(capsys, test_settings, expected_output) -> None:
-    """Test menu.display_menu with  fixtures."""
-    with patch('game_of_life.menu.get_all_presets') as mock_get_all_presets:
+def test_preset_menu_with_fixture(capsys, test_settings, expected_output) -> None:
+    """Test menu.preset_menu with  fixtures.
+
+    This only tests the printed menu. The return value is ignored
+    and will be tested separately.
+    """
+    with (patch('game_of_life.menu.get_all_presets') as mock_get_all_presets,
+          patch('game_of_life.menu.get_user_preset_choice') as _):
         mock_get_all_presets.return_value = test_settings
-        display_menu()
+        preset_menu()
         captured = capsys.readouterr()
         assert captured.err == ''
         assert captured.out.strip() == expected_output
 
 
-def test_display_menu_with_presets(capsys) -> None:
+def test_preset_menu_with_presets(capsys) -> None:
     """Test menu display with actual Presets.
 
     Expected output of each printed line in the form:
@@ -39,20 +44,25 @@ def test_display_menu_with_presets(capsys) -> None:
     Examples
     --------
         Printed line => '2. Beacon'
+
+    Notes
+    -----
+    This only tests the printed menu. The return value is ignored
     """
-    display_menu()
-    captured = capsys.readouterr()
-    # Check 1: no error strings.
-    assert captured.err == ''
-    # Check 2: Correctly formatted strings
-    assert captured.out != ''
-    out = captured.out.strip()
-    for line in out.split('\n'):
-        # Line should have two parts: 'index. name'.
-        # `name` may be any non-empty string
-        try:
-            idx_str, _ = line.split(' ', 1)
-            assert idx_str[:-1].isnumeric()
-            assert idx_str[-1] == '.'
-        except ValueError as exc:
-            raise AssertionError(f"Unexpected format in line: '{line}'") from exc
+    with patch('game_of_life.menu.get_user_preset_choice') as _:
+        preset_menu()
+        captured = capsys.readouterr()
+        # Check 1: no error strings.
+        assert captured.err == ''
+        # Check 2: Correctly formatted strings
+        assert captured.out != ''
+        out = captured.out.strip()
+        for line in out.split('\n'):
+            # Line should have two parts: 'index. name'.
+            # `name` may be any non-empty string
+            try:
+                idx_str, _ = line.split(' ', 1)
+                assert idx_str[:-1].isnumeric()
+                assert idx_str[-1] == '.'
+            except ValueError as exc:
+                raise AssertionError(f"Unexpected format in line: '{line}'") from exc
