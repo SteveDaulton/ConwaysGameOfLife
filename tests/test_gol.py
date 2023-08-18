68f35c0f76b01c629d5cab9801d7e962d9777827
Steve Daulton <stevedaulton@gmail.com>
2023-08-18 19:20:04 +0100

    Move validation tests into their own file.

--- tests/test_gol.py
+++ tests/test_gol.py
@@ -8,10 +8,6 @@ from game_of_life.gol import (Universe,
                               GameOfLifeUI,
                               get_all_presets)
 from game_of_life.custom_types import Point, Preset, Size
-from game_of_life.validate import (
-    valid_refresh_rate_string,
-    valid_preset_id_string,
-    preset_id_range)
 from game_of_life.constants import DEFAULTS
 
 
@@ -103,55 +99,6 @@ def test_universe_attribute_values(universe_singleton, attribute, expected_val):
     universe = universe_singleton
     attr = getattr(universe, attribute)
     assert attr == expected_val
-
-
-def test_valid_preset_id() -> None:
-    """Test __main__.valid_preset."""
-    # TODO: Doesn't belong in this file.
-    valid_indices = range(preset_id_range())
-    # Case 1: Valid presets.
-    for idx in valid_indices:
-        assert valid_preset_id_string(f'{idx}') == idx
-    # Case 2: Invalid preset IDs below 0.
-    with pytest.raises(ValueError) as exc_info:
-        valid_preset_id_string("-1")
-    assert "is not a valid preset ID" in str(exc_info.value)
-    # Case 3: Invalid preset IDs above maximum index.
-    with pytest.raises(ValueError) as exc_info:
-        valid_preset_id_string(str(preset_id_range() + 1))
-    assert "is not a valid preset ID" in str(exc_info.value)
-    # Case 4: Non-integer input
-    with pytest.raises(ValueError) as exc_info:
-        valid_preset_id_string("not_an_integer")
-    assert "not_an_integer" in str(exc_info.value)
-
-
-def test_valid_refresh_rate() -> None:
-    """Test __main__.valid_refresh_rate.
-
-    Refresh rate is fastest when waiting before refresh is zero (disabled).
-    `slowest` is fairly arbitrary, but should match __main__.valid_refresh_rate().
-    """
-    # TODO: Doesn't belong in this file.
-    fastest = 0
-    slowest = 10
-    # Case 1: Valid values.
-    delta = 1e-9
-    for val in range(fastest, 10 * slowest):
-        float_val = val / 10.0
-        # Being cautious about float precision, though probably not necessary.
-        assert valid_refresh_rate_string(f'{float_val}') - float_val < delta
-    # Case 2: Out of range values.
-    with pytest.raises(ValueError) as exc_info:
-        valid_refresh_rate_string(f'{fastest - 0.01}')
-    assert 'Refresh rate must be between' in str(exc_info.value)
-    with pytest.raises(ValueError) as exc_info:
-        valid_refresh_rate_string(f'{slowest + 0.01}')
-    assert 'Refresh rate must be between' in str(exc_info.value)
-    # Case 3: Non-number input
-    with pytest.raises(ValueError) as exc_info:
-        valid_refresh_rate_string('not a number')
-    assert 'not a number' in str(exc_info.value)
 
 
 def test_init_cells(universe_singleton):
---
+++ tests/test_validate.py
@@ -0,0 +1,55 @@
+"""Tests for validate.py."""
+
+import pytest
+
+from game_of_life.validate import (
+    valid_refresh_rate_string,
+    valid_preset_id_string,
+    preset_id_range)
+
+
+def test_valid_preset_id() -> None:
+    """Test __main__.valid_preset."""
+    valid_indices = range(preset_id_range())
+    # Case 1: Valid presets.
+    for idx in valid_indices:
+        assert valid_preset_id_string(f'{idx}') == idx
+    # Case 2: Invalid preset IDs below 0.
+    with pytest.raises(ValueError) as exc_info:
+        valid_preset_id_string("-1")
+    assert "is not a valid preset ID" in str(exc_info.value)
+    # Case 3: Invalid preset IDs above maximum index.
+    with pytest.raises(ValueError) as exc_info:
+        valid_preset_id_string(str(preset_id_range() + 1))
+    assert "is not a valid preset ID" in str(exc_info.value)
+    # Case 4: Non-integer input
+    with pytest.raises(ValueError) as exc_info:
+        valid_preset_id_string("not_an_integer")
+    assert "not_an_integer" in str(exc_info.value)
+
+
+def test_valid_refresh_rate() -> None:
+    """Test __main__.valid_refresh_rate.
+
+    Refresh rate is fastest when waiting before refresh is zero (disabled).
+    `slowest` is fairly arbitrary, but should match __main__.valid_refresh_rate().
+    """
+    fastest = 0
+    slowest = 10
+    # Case 1: Valid values.
+    delta = 1e-9
+    for val in range(fastest, 10 * slowest):
+        float_val = val / 10.0
+        # Being cautious about float precision, though probably not necessary.
+        assert valid_refresh_rate_string(f'{float_val}') - float_val < delta
+    # Case 2: Out of range values.
+    with pytest.raises(ValueError) as exc_info:
+        valid_refresh_rate_string(f'{fastest - 0.01}')
+    assert 'Refresh rate must be between' in str(exc_info.value)
+    with pytest.raises(ValueError) as exc_info:
+        valid_refresh_rate_string(f'{slowest + 0.01}')
+    assert 'Refresh rate must be between' in str(exc_info.value)
+    # Case 3: Non-number input
+    with pytest.raises(ValueError) as exc_info:
+        valid_refresh_rate_string('not a number')
+    assert 'not a number' in str(exc_info.value)
